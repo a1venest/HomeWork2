ac7ab5e2be473ae643c21bf5ed466b5bcb0532c0
Aivenest <aivenest@gmail.com>
2022-09-27 10:46:07 +0500

    Exercise01 compleated

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Dec(int number)
+{
+    int Decimal = number / 10;
+    int Decimal2 = Decimal % 10;
+    return Decimal2;
+}
+
+Console.WriteLine(Dec(number));
