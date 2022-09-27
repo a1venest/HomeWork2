ae29cb8e1edb48a92363780de20cdeed4b461c76
Aivenest <aivenest@gmail.com>
2022-09-27 11:17:40 +0500

    Exercise02 compleated

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ThirdNumber(int number)
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+
+    int unit = number % 10;
+    return unit;
+}
+
+Console.WriteLine(ThirdNumber(number));
