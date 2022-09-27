4505080b9881420694bb0b8890fbe4d777dc796c
Aivenest <aivenest@gmail.com>
2022-09-27 12:30:54 +0500

    Exercise003 Compleated

--- Exercise03/Program.cs
+++ Exercise03/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер дня недели от 1 до 7, где 1 - это понедельник, а 7 - это воскресенье:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void DayOfWeek(int day)
+{
+    if (day ==6)
+    {
+        Console.WriteLine("Суббота, Выходной!");
+    }
+    else if (day ==7)
+    {
+        Console.WriteLine("Воскресенье, выходной! Но завтра на работу(");
+    }
+    else
+    {
+        Console.WriteLine("Работать негры, еще не выходной!");
+    }
+}
+
+DayOfWeek(day);
+
+
+
